e9fa3cb6a5938af2e6e3f5ddf537a6f443211044
karolziober <karolziober@gmail.com>
2026-03-04 19:30:23 +0100

    updt

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
 "use strict";
+
+class Main {
+  constructor() {
+    // BUTTONS
+    this.menuOpenBtn = document.getElementById("nav-open");
+    this.menuCloseBtn = document.getElementById("nav-close");
+    this.menuBtns = document.querySelectorAll(".nav__btn");
+
+    // ELEMENTS
+    this.menu = document.querySelector(".nav__panel");
+
+    this.init();
+  }
+
+  init() {
+    this.menuToggle();
+  }
+
+  menuToggle() {
+    this.menuBtns.forEach((btn) => {
+      btn.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        this.menu.classList.toggle("nav__panel--open");
+        this.menuOpenBtn.classList.toggle("nav__open--hidden");
+      });
+    });
+  }
+}
+
+new Main();
